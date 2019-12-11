095d75af70cd1e2db9828ebafaf3a13720518212
rumylera <45111115+rumylera@users.noreply.github.com>
2019-12-11 17:49:58 +0300

    Everything till Sparse (works)

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -8,8 +8,32 @@ import java.util.List;
  * Created by artemaliev on 07/09/15.
  */
 public class IntSort {
+  public static void swap (int i, int j, int array[])
+  {
+    int temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
   public static void sort (int array[]) {
-    Arrays.sort(array);
+    int length = array.length;
+    int gap = length;
+    boolean check = true;
+    while(gap>1||check)
+    {
+      if(gap>1)
+      {
+        gap=(int)(gap/1.3);
+      }
+      check = false;
+      for(int i=0; i<length-gap;i++)
+      {
+        if(array[i]>array[i+gap])
+        {
+          swap(i, i+gap, array);
+          check = true;
+        }
+      }
+    }
   }
 
   public static void sort (List<Integer> list) {
